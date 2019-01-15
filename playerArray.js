8ce946c56cf189eb9a4e61a718c94cdb0864545f
wavefly <samsonmonday35@gmail.com>
2019-01-15 09:41:28 +0000

    first assignment

---
+++ playerArray.js
@@ -0,0 +1,22 @@
+//using the array
+var myPlayer
+//array list
+myPlayer = ["Michael", "Male", 20, 189, "Chelsea", "Wolf Region FC", "Striker", "$135", 85, 2, 5, "active", true];
+//the header
+document.write('<h2>My Best Player Details</h2>')
+// the player details
+('Player name:<span style = "color: grey;">-----------------------</span>:', myPlayer[0], '<br>')
+('Player gender:<span style = "color: grey;">---------------------</span>:', myPlayer[1], '<br>')
+('Player gender:<span style = "color: grey;">---------------------</span>:', myPlayer[1], '<br>')
+('Player age:<span style = "color: grey;">------------------------</span>:', myPlayer[2], '<br>')
+('Player height:<span style = "color: grey;">---------------------</span>:', myPlayer[3], '<br>')
+('Player previous-club:<span style = "color: grey;">-------------</span>:', myPlayer[4], '<br>')
+('Player present-club:<span style = "color: grey;">---------------</span>:', myPlayer[5], '<br>')
+//note: only print the above
+('Player position:<span style = "color: grey;">-------------------</span>:', myPlayer[6], '<br>')
+('Player worth:<span style = "color: grey;">-----------------------</span>:', myPlayer[7], '<br>')
+('Player total-score:<span style = "color: grey;">-----------------</span>:', myPlayer[8], '<br>')
+('Player total red-card obtain:<span style = "color: grey;">-------</span>:', myPlayer[9], '<br>')
+('Player total red-card obtain:<span style = "color: grey;">-------</span>:', myPlayer[10], '<br>')
+('Player Status:<span style = "color: grey;">-----------------------</span>:', myPlayer[11], '<br>')
+('Player on vacation:<span style = "color: grey;">-----------------</span>:', myPlayer[12], '<br>')
---
+++ var.js
@@ -0,0 +1,28 @@
+//using variable to describe the player details
+var playerName = "Michael"
+var playerSex = "male"
+var playerAge = 20
+var playerHeight = 189
+var playerPreviousClub = "Chelsea"
+var playerPresentClub = "Wolf Region FC"
+var playingPosition = "Striker"
+var playerWorth = "$135"
+var totalScore = 85
+var totalRedCardObtain = 2
+var totalYellowCardObtain = 5
+var playerOnVacation = false
+//the header
+document.write('<h2>My Best Player Of The Year</h2>')
+//the print statement
+document.write('Player name<span style = "color: grey;">----------------</span>: ', playerName, '<br>')
+document.write('Player sex<span style = "color: grey;">------------------</span>: ', playerSex, '<br>')
+document.write('Player age<span style = "color: grey;">------------------</span>: ', playerAge, '<br>')
+document.write('Player height<span style = "color: grey;">---------------</span>: ', playerHeight, '<br>')
+document.write('Player previous-club<span style = "color: grey;">-------</span>: ', playerPreviousClub, '<br>')
+document.write('Player present-club<span style = "color: grey;">--------</span>: ', playerPresentClub, '<br>')
+document.write('Player position<span style = "color: grey;">------------</span>: ', playerPosition, '<br>')
+document.write('Player worth<span style = "color: grey;">---------------</span>: ', playerWorth, '<br>')
+document.write('Player total-score<span style = "color: grey;">---------</span>: ', totalScore, '<br>')
+document.write('Player total red-card obtain<span style = "color: grey;">-</span>: ', totalRedCardObtain, '<br>')
+document.write('Player total red-card obtain<span style = "color: grey;">-</span>: ', totalYellowCardObtain, '<br>')
+document.write('Player on vacation<span style = "color: grey;">-----------</span>: ', playerOnVacation, '<br>')
